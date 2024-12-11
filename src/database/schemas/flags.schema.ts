23cbc1fbf72970e96bc5dfde0fcac617d0ee24a0
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 00:32:16 +0200

    schemas<>declared flagged_id prop in Flag

--- src/database/schemas/flags.schema.ts
+++ src/database/schemas/flags.schema.ts
@@ -5,4 +5,6 @@ export type FlagDocument = Flag & Document;
 
 @Schema({ timestamps: true })
 export class Flag {
+  @Prop({ type: Types.ObjectId, required: true })
+  flagged_id: Types.ObjectId;
 export const FlagSchema = SchemaFactory.createForClass(Flag);
