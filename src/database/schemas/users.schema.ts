03629972e70fab995015c78dc406492d30e9ebe6
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 00:24:21 +0200

    schemas<>declared a User schema decorator class

--- src/database/schemas/users.schema.ts
+++ src/database/schemas/users.schema.ts
@@ -2,3 +2,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
 export type UserDocument = User & Document;
+
+@Schema({ timestamps: true })
+export class User {
+}
