ec5ecc84ec6ae6116f1220f23599e32c4aef7045
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 00:26:48 +0200

    schemas<>declared a user_type prop in User

--- src/database/schemas/users.schema.ts
+++ src/database/schemas/users.schema.ts
@@ -13,4 +13,7 @@ export class User {
 
   @Prop({ required: true })
   password: string;
+
+  @Prop({ required: true, enum: ['barterer', 'broker', 'admin'] })
+  user_type: 'barterer' | 'broker' | 'admin';
 export const UserSchema = SchemaFactory.createForClass(User);
