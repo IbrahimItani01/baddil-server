aa5332aa640e0894b40f1a10ce1e14eaded91214
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 00:49:25 +0200

    schemas<>declared users_involved prop as a array of objectId in Dispute

--- src/database/schemas/disputes.schema.ts
+++ src/database/schemas/disputes.schema.ts
@@ -2,8 +2,11 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
 export type DisputeDocument = Dispute & Document;
+
 @Schema({ timestamps: true })
 export class Dispute {
+  @Prop({ type: [Types.ObjectId], ref: 'User', required: true })
+  users_involved: Types.ObjectId[];
 }
 
 export const DisputeSchema = SchemaFactory.createForClass(Dispute);
