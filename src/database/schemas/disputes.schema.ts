46b7c7c9de6317ccfc4160d68ab2b12b192d4891
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 00:48:10 +0200

    schemas<>created a disputes schema class

--- src/database/schemas/disputes.schema.ts
+++ src/database/schemas/disputes.schema.ts
@@ -2,3 +2,5 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
 export type DisputeDocument = Dispute & Document;
+@Schema({ timestamps: true })
+export class Dispute {
