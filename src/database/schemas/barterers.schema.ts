819287c8d8ed9656dbdc0a897e8601759c92008a
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 17:12:27 +0200

    schemas<>declared pro_status prop

--- src/database/schemas/barterers.schema.ts
+++ src/database/schemas/barterers.schema.ts
@@ -7,6 +7,22 @@ export type BartererDocument = Barterer & Document;
 export class Barterer {
   @Prop({ type: Types.ObjectId, ref: 'User', required: true })
   user_id: Types.ObjectId;
+
+  @Prop({
+    type: {
+      is_pro: { type: Boolean, required: true, default: false },
+      activated_on: { type: Date, required: false },
+      expires_on: { type: Date, required: false },
+      plan_id: { type: Types.ObjectId, ref: 'Company', required: true },
+    },
+  })
+  pro_status: {
+    is_pro: boolean;
+    activated_on?: Date;
+    expires_on?: Date;
+    plan_id: Types.ObjectId;
+  };
+
 }
 
 export const BartererSchema = SchemaFactory.createForClass(Barterer);
