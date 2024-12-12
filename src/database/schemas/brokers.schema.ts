31b4f10942b360527a816f0b9fda4a3187262f05
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 02:37:55 +0200

    schemas<>created a Broker schema

--- src/database/schemas/brokers.schema.ts
+++ src/database/schemas/brokers.schema.ts
@@ -2,3 +2,6 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
 export type BrokerDocument = Broker & Document;
+
+@Schema({ timestamps: true })
+export class Broker {
