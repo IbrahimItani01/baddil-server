6f14850c13820a5d50d6ee80eed4c4c30fe661f0
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 17:11:02 +0200

    schemas<>declared  Barterer class schema

--- src/database/schemas/barterers.schema.ts
+++ src/database/schemas/barterers.schema.ts
@@ -2,3 +2,5 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
 export type BartererDocument = Barterer & Document;
+@Schema({ timestamps: true })
+export class Barterer {
