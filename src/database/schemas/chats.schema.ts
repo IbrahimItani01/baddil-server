c949cd370b42e191d3722e4e8f25e2aad4888460
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 17:34:09 +0200

    schemas<>declared Chat schema class

--- src/database/schemas/chats.schema.ts
+++ src/database/schemas/chats.schema.ts
@@ -2,3 +2,6 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
 export type ChatDocument = Chat & Document;
+
+@Schema({ timestamps: true })
+export class Chat {
