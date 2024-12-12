40925429f7f332ff9af94b707c00139b4e0e5c10
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 17:12:00 +0200

    schemas<>declared user_id prop

--- src/database/schemas/barterers.schema.ts
+++ src/database/schemas/barterers.schema.ts
@@ -5,6 +5,8 @@ export type BartererDocument = Barterer & Document;
 
 @Schema({ timestamps: true })
 export class Barterer {
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  user_id: Types.ObjectId;
 }
 
 export const BartererSchema = SchemaFactory.createForClass(Barterer);
