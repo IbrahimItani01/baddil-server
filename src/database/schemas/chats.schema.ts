728601949017ec988efb9c7a4cb9e94f7e5ee021
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 17:35:40 +0200

    schemas<>declared messages prop array of objects

--- src/database/schemas/chats.schema.ts
+++ src/database/schemas/chats.schema.ts
@@ -8,6 +8,30 @@ export class Chat {
   @Prop({ type: [Types.ObjectId], ref: 'User', required: true })
   users_involved: Types.ObjectId[];
 
+  @Prop({
+    type: [
+      {
+        message_id: { type: Types.ObjectId, required: true },
+        sender: { type: Types.ObjectId, ref: 'User', required: true },
+        content: { type: String, required: true },
+        sent_date: { type: Date, default: Date.now, required: true },
+        status: {
+          type: String,
+          enum: ['sent', 'read', 'received'],
+          default: 'sent',
+          required: true,
+        },
+      },
+    ],
+    required: true,
+  })
+  messages: {
+    message_id: Types.ObjectId;
+    sender: Types.ObjectId;
+    content: string;
+    sent_date: Date;
+    status: 'sent' | 'read' | 'received';
+  }[];
 }
 
 export const ChatSchema = SchemaFactory.createForClass(Chat);
