af1427e4b7b1b67e03629de8a90cb07ad789c4ce
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 02:23:12 +0200

    main<>wrapped app listen in a try-catch logic with logger log

--- src/main.ts
+++ src/main.ts
@@ -7,5 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService); 
   const port = configService.get('PORT', 3000); 
+
+  try {
+    await app.listen(port);
+    Logger.log(`🚀 Server started on http://localhost:${port}`); 
+  } catch (error) {
+    Logger.error('❌ Server failed to start:', error);
+  }
 }
 bootstrap();
