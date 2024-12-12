00b21d0c6e5382783273e4570f211576d01d0e40
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-12 14:56:52 +0200

    schemas<>created Company schema

--- src/database/schemas/company.schema.ts
+++ src/database/schemas/company.schema.ts
@@ -2,3 +2,6 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
 
 export type CompanyDocument = Company & Document;
+
+@Schema({ timestamps: true })
+export class Company {
