0ca3b5bca5a0f6bee29748cc6e98e3ad20a8b574
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-14 01:54:19 +0200

    services<>created BrokersService class

---
+++ src/modules/brokers/brokers.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Broker, BrokerDocument } from 'src/database/schemas/brokers.schema';
+
+@Injectable()
+export class BrokersService {
