7f0b2e4fe45e5e131ae0df8b38617f8f7392e725
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-14 00:27:22 +0200

    services<>declared UsersService with constructor to inject user model

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -3,3 +3,10 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateUserDto } from './dto/createUser.dto';
 import { User, UserDocument } from 'src/database/schemas/users.schema';
+
+@Injectable()
+export class UsersService {
+    constructor(
+        @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+      ) {}
+}
