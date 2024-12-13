e4d07f3a969f4a13c806c958adbd092284837e4d
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-13 04:35:15 +0200

    schemas<>exported the sub-schemas for barters schemas from a separate file

---
+++ src/database/subSchemas/barters.subSchema.ts
@@ -0,0 +1,42 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+import { ReviewSide, MeetupStatus } from 'src/utils/enums.utils'; // Correct usage of enums for sub-schemas
+
+@Schema()
+export class Review {
+  @Prop({
+    type: String,
+    enum: Object.values(ReviewSide),
+    required: true,
+  })
+  side: ReviewSide;
+
+  @Prop({ type: String, required: true })
+  review_text: string;
+}
+
+export const ReviewSchema = SchemaFactory.createForClass(Review);
+
+@Schema()
+export class Meetup {
+  @Prop({ type: Types.ObjectId, ref: 'Meetup', required: true })
+  meetup_id: Types.ObjectId;
+
+  @Prop({
+    type: String,
+    enum: Object.values(MeetupStatus),
+    required: true,
+  })
+  status: MeetupStatus;
+
+  @Prop({ type: String, required: true })
+  location: string;
+
+  @Prop({ type: String, required: true })
+  qr_code: string;
+
+  @Prop({ type: Date, required: true })
+  date: Date;
+}
+
+export const MeetupSchema = SchemaFactory.createForClass(Meetup);
