7ff47827e9163411692ad67c16a11b393e5c611c
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-13 04:06:30 +0200

    schemas<>replaced inline enums with imported ones

--- src/database/schemas/barters.schema.ts
+++ src/database/schemas/barters.schema.ts
@@ -1,36 +1,46 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Types } from 'mongoose';
+import {
+  BarterStatus,
+  Rating,
+  ReviewSide,
+  MeetupStatus,
+} from '../../utils/enums.utils';
 
 export type BarterDocument = Barter & Document;
 
-@Schema({ timestamps: true })  
+@Schema({ timestamps: true })
 export class Barter {
   @Prop({
     type: String,
-    enum: ['ongoing', 'completed', 'rejected'],
-    default: 'ongoing',
+    enum: Object.values(BarterStatus),
+    default: BarterStatus.Ongoing,
     required: true,
   })
-  status: 'ongoing' | 'completed' | 'rejected';
+  status: BarterStatus;
 
   @Prop({ type: [Types.ObjectId], ref: 'Item', required: true })
-  initiator_items: Types.ObjectId[]; 
+  initiator_items: Types.ObjectId[];
 
   @Prop({ type: [Types.ObjectId], ref: 'Item', required: true })
-  receiver_items: Types.ObjectId[]; 
+  receiver_items: Types.ObjectId[];
 
   @Prop({ type: Date, required: false })
-  completed_at: Date; 
+  completed_at: Date;
 
-  @Prop({ type: Number, enum: [1, 2, 3, 4, 5], required: false })
-  rating: 1 | 2 | 3 | 4 | 5; 
+  @Prop({
+    type: Number,
+    enum: Object.values(Rating),
+    required: false,
+  })
+  rating: Rating;
 
   @Prop({
     type: [
       {
         side: {
           type: String,
-          enum: ['initiator', 'receiver'],
+          enum: Object.values(ReviewSide),
           required: true,
         },
         review_text: { type: String, required: true },
@@ -38,28 +48,28 @@ export class Barter {
     ],
     required: false,
   })
-  reviews: { side: 'initiator' | 'receiver'; review_text: string }[]; 
+  reviews: { side: ReviewSide; review_text: string }[];
 
   @Prop({
     type: {
       meetup_id: { type: Types.ObjectId, ref: 'Meetup', required: true },
       status: {
         type: String,
-        enum: ['scheduled', 'ongoing', 'cancelled', 'success'],
+        enum: Object.values(MeetupStatus),
         required: true,
       },
       location: { type: String, required: true },
-      qr_code: { type: String, required: true }, 
-      date: { type: Date, required: true }, 
+      qr_code: { type: String, required: true },
+      date: { type: Date, required: true },
     },
     required: false,
   })
   meetup: {
-    meetup_id: Types.ObjectId; 
-    status: 'scheduled' | 'ongoing' | 'cancelled' | 'success'; 
-    location: string; 
-    qr_code: string; 
-    date: Date; 
+    meetup_id: Types.ObjectId;
+    status: MeetupStatus;
+    location: string;
+    qr_code: string;
+    date: Date;
   };
 }
 
