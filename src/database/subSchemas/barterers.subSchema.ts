474699ba0e87bd92967e5cfef127e072b0889dd7
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-13 04:35:25 +0200

    schemas<>exported the sub-schemas for barterers schemas from a separate file

---
+++ src/database/subSchemas/barterers.subSchema.ts
@@ -0,0 +1,62 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+import { AutoTradeStatus, ItemCondition } from 'src/utils/enums.utils';
+
+@Schema()
+export class WalletItem {
+  @Prop({ type: Types.ObjectId, ref: 'Item', required: true })
+  item_id: Types.ObjectId;
+
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true })
+  category: string;
+
+  @Prop({
+    type: String,
+    enum: Object.values(ItemCondition),
+    required: true,
+  })
+  condition: ItemCondition;
+
+  @Prop({ type: String, required: false })
+  description: string;
+
+  @Prop({ type: Number, required: true })
+  estimated_value: number;
+
+  @Prop({ type: Date, default: Date.now, required: true })
+  created_at: Date;
+
+  @Prop({ type: [String], required: false })
+  images: string[];
+}
+
+export const WalletItemSchema = SchemaFactory.createForClass(WalletItem);
+@Schema()
+export class AutoTradeData {
+  @Prop({ type: Types.ObjectId })
+  _id: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'Item', required: true })
+  item_id: Types.ObjectId;
+
+  @Prop({
+    type: String,
+    enum: Object.values(AutoTradeStatus),
+    required: true,
+  })
+  status: AutoTradeStatus;
+
+  @Prop({ type: Date, default: Date.now, required: true })
+  started_on: Date;
+
+  @Prop({ type: Date, required: false })
+  finalized_on: Date;
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Chat', required: false }] })
+  chats: Types.ObjectId[];
+}
+
+export const AutoTradeDataSchema = SchemaFactory.createForClass(AutoTradeData);
