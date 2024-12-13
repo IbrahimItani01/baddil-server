32d3bb3409ed7583bf7a97d722ab4c5dd608b83a
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-14 00:26:32 +0200

    services<>imported necessary methods in users service

---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/createUser.dto';
+import { User, UserDocument } from 'src/database/schemas/users.schema';
