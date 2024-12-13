f7a85a9eb8484447bd7daaae17c4682eed037f7d
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-14 00:20:34 +0200

    main<>used Global pipes for validation

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService); 
-  const port = configService.get('PORT', 3000); 
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT', 3000);
+  app.useGlobalPipes(new ValidationPipe());
 
   try {
     await app.listen(port);
-    Logger.log(`🚀 Server started on http://localhost:${port}`); 
+    Logger.log(`🚀 Server started on http://localhost:${port}`);
   } catch (error) {
     Logger.error('❌ Server failed to start:', error);
   }
