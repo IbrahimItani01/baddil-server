0152c19b13a08c33fc77f2a35663775cb6bd7804
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-13 06:11:26 +0200

    schemas<>imported enums in brokers subSchema

--- src/database/subSchemas/brokers.subSchema.ts
+++ src/database/subSchemas/brokers.subSchema.ts
@@ -1,5 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Types } from 'mongoose';
+import { ClientStatusEnum, RatingEnum } from 'src/utils/enums.utils';
 
 @Schema()
 export class ClientGoal {
