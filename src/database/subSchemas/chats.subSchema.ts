8d6be41d9a2903779138775953f6aeedf9c43747
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-13 04:35:33 +0200

    schemas<>exported the sub-schemas for chats schemas from a separate file

---
+++ src/database/subSchemas/chats.subSchema.ts
@@ -0,0 +1,28 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+import { MessageStatus } from 'src/utils/enums.utils';
+
+@Schema()
+export class Message {
+  @Prop({ type: Types.ObjectId, required: true })
+  message_id: Types.ObjectId;
+
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  sender: Types.ObjectId;
+
+  @Prop({ type: String, required: true })
+  content: string;
+
+  @Prop({ type: Date, default: Date.now, required: true })
+  sent_date: Date;
+
+  @Prop({
+    type: String,
+    enum: Object.values(MessageStatus),
+    default: MessageStatus.Sent,
+    required: true,
+  })
+  status: MessageStatus;
+}
+
+export const MessageSchema = SchemaFactory.createForClass(Message);
