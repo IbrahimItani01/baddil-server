f7936d0722df0c1a7249deb2cc4b038627956ad4
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-14 02:00:08 +0200

    services<>imported necessary packages in barterers module

---
+++ src/modules/barterers/barterers.service.ts
@@ -0,0 +1,5 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { Barterer, BartererDocument } from 'src/database/schemas/barterers.schema';
+
