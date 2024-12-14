9f212436f67fcad85b753ef1fae72850b7247559
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-14 02:07:33 +0200

    services<>handled if  user email was not found

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -56,3 +56,9 @@ export class AuthService {
   }
   async login(email: string, password: string) {
     
+    const user = await this.usersService.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('Invalid email or password');
+    }
+
+    
