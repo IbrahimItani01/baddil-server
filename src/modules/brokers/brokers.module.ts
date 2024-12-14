f15fe549cbe192e2804036ea7f597e2bb2bf2c2f
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-14 03:14:03 +0200

    modules<>imported MongooseModule for broker schema

--- src/modules/brokers/brokers.module.ts
+++ src/modules/brokers/brokers.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { BrokersController } from './brokers.controller';
 import { BrokersService } from './brokers.service';
+import { Broker, BrokerSchema } from 'src/database/schemas/brokers.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Broker.name, schema: BrokerSchema }]), 
+  ],
   controllers: [BrokersController],
   providers: [BrokersService],
-  exports: [BrokersService],
+  exports: [BrokersService], 
 })
 export class BrokersModule {}
