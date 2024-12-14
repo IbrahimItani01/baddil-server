720c6a8337aeee51b7e9744ded689e0380f1cacc
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-14 16:12:13 +0200

    dtos<>deleted dto files

--- src/modules/users/dto/loginUser.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsString } from 'class-validator';
-
-export class LoginUserDto {
-  @IsEmail({}, { message: 'Invalid email address' })
-  readonly email: string;
-
-  @IsString()
-  readonly password: string;
-}
