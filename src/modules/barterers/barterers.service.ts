6f2d931e1e23328f7e8df1474e1e950a205bbaa4
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-14 02:00:30 +0200

    services<>injected barterer model in the barterers service

--- src/modules/barterers/barterers.service.ts
+++ src/modules/barterers/barterers.service.ts
@@ -3,3 +3,9 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { Barterer, BartererDocument } from 'src/database/schemas/barterers.schema';
 
+@Injectable()
+export class BarterersService {
+  constructor(
+    @InjectModel(Barterer.name) private bartererModel: Model<BartererDocument>,
+  ) {}
+
