096f5ee6c811e32e2851e721598e805f88f9e5bb
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-15 20:34:48 +0200

    services<>imported packages in auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,14 +2,16 @@ import {
   Injectable,
   BadRequestException,
   UnauthorizedException,
+  Inject,
 } from '@nestjs/common';
-import { CreateUserDto } from 'src/modules/users/dto/createUser.dto';
+import { ConfigService } from '@nestjs/config';
+import { UsersService } from 'src/modules/users/users.service';
 import { BarterersService } from 'src/modules/barterers/barterers.service';
 import { BrokersService } from 'src/modules/brokers/brokers.service';
-import { UsersService } from 'src/modules/users/users.service';
 import { Types } from 'mongoose';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as admin from 'firebase-admin';
+import axios from 'axios';
 
 @Injectable()
 export class AuthService {
