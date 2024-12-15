dab642e1bc121508cd187c3eea549f0908f629d8
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 01:00:22 +0200

    seeders<>embedded the generate password function in users seeder

--- src/database/seeders/users/users.seeder.ts
+++ src/database/seeders/users/users.seeder.ts
@@ -2,10 +2,11 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { faker } from '@faker-js/faker';
 import { UserTypeEnum } from 'src/utils/enums.utils';
+import { generateValidPassword } from 'src/utils/seeders/users.functions';
 
 @Injectable()
 export class UserSeeder {
-  private readonly registerUrl = 'http://localhost:8800/api/auth/register'; // API URL
+  private readonly registerUrl = 'http://localhost:8800/api/auth/register';
 
   async seedUsers(count: number): Promise<void> {
     const promises = Array.from({ length: count }).map(async () => {
@@ -14,6 +15,21 @@ export class UserSeeder {
         email: faker.internet.email(),
         user_type: faker.helpers.arrayElement(Object.values(UserTypeEnum)),
         profile_picture: faker.image.avatar(),
-        password: faker.internet.password(),
+        password: generateValidPassword(),
       };
 
+      try {
+        const response = await axios.post(this.registerUrl, fakeUser);
+        console.log(`User registered:`, response.data.data);
+      } catch (error) {
+        console.error(
+          `Failed to register user:`,
+          error.response?.data || error.message,
+        );
+      }
+    });
+
+    await Promise.all(promises);
+    console.log(`${count} users have been seeded!`);
+  }
+}
