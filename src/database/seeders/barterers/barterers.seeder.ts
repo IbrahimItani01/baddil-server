744e093bb266b33cf5a746c46c58fdc639b70d44
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 01:02:45 +0200

    seeders<>constructed BarterersSeeder that injects required models

--- src/database/seeders/barterers/barterers.seeder.ts
+++ src/database/seeders/barterers/barterers.seeder.ts
@@ -6,3 +6,12 @@ import { Barterer, BartererDocument } from 'src/database/schemas/barterers.schem
 import { User, UserDocument } from 'src/database/schemas/users.schema';
 import { AutoTradeStatusEnum, ItemConditionEnum } from 'src/utils/enums.utils';
 import { Broker, BrokerDocument } from 'src/database/schemas/brokers.schema';
+
+@Injectable()
+export class BarterersSeeder {
+  constructor(
+    @InjectModel(Barterer.name) private readonly bartererModel: Model<BartererDocument>,
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    @InjectModel(Broker.name) private readonly brokerModel: Model<BrokerDocument>,
+  ) {}
+
