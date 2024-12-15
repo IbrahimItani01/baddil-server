7a3bba55f52645e03f41552cd73e966af019d3e6
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 00:47:22 +0200

    seeders<>declared seedUsers method that fills the user document using faker

--- src/database/seeders/users/users.seeder.ts
+++ src/database/seeders/users/users.seeder.ts
@@ -7,3 +7,13 @@ import { UserTypeEnum } from 'src/utils/enums.utils';
 export class UserSeeder {
   private readonly registerUrl = 'http://localhost:8800/api/auth/register'; // API URL
 
+  async seedUsers(count: number): Promise<void> {
+    const promises = Array.from({ length: count }).map(async () => {
+      const fakeUser = {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        user_type: faker.helpers.arrayElement(Object.values(UserTypeEnum)),
+        profile_picture: faker.image.avatar(),
+        password: faker.internet.password(),
+      };
+
