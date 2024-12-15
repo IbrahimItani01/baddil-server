c616c600bf7517c3cd00084f0f01de79ad33f914
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 00:45:38 +0200

    seeders<>declared useSeeder class

--- src/database/seeders/users/users.seeder.ts
+++ src/database/seeders/users/users.seeder.ts
@@ -2,3 +2,6 @@ import { Injectable } from '@nestjs/common';
 import axios from 'axios';
 import { faker } from '@faker-js/faker';
 import { UserTypeEnum } from 'src/utils/enums.utils';
+
+@Injectable()
+export class UserSeeder {
