1d8a9469dd8e8e097cccb630026f68733d8e1cf7
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 17:05:02 +0200

    seeders<>stored user_types ids

--- src/database/seeders/disputes/disputes.seeder.ts
+++ src/database/seeders/disputes/disputes.seeder.ts
@@ -17,3 +17,16 @@ export class DisputesSeeder {
     return this.disputeModel;
   }
 
+  async seed() {
+    const existingDisputes = await this.disputeModel.find();
+    if (existingDisputes.length > 0) {
+      return;
+    }
+
+    const barterers = await this.userModel
+      .find({ user_type: 'barterer' })
+      .select('_id');
+    const admins = await this.userModel
+      .find({ user_type: 'admin' })
+      .select('_id');
+
