fdfcc852dbe8dd4fa9db2a5db0b4ca37615ce790
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 17:19:32 +0200

    seeders<>imported necessary modules in barters seeder

---
+++ src/database/seeders/barters/barters.seeder.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { faker } from '@faker-js/faker';
+import { BarterStatusEnum, RatingEnum } from '../../../utils/enums.utils';
+import { MeetupStatusEnum, ReviewSideEnum } from '../../../utils/enums.utils';
+import { v4 as uuidv4 } from 'uuid'; 
+import { Barter, BarterDocument } from '../../schemas/barters.schema';
+
