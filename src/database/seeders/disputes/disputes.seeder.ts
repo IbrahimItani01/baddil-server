b3b9b377c9e35c7fc1ee94fbf1b327267dd6a74a
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 17:03:36 +0200

    seeders<>constructed DisputesSeeder class

--- src/database/seeders/disputes/disputes.seeder.ts
+++ src/database/seeders/disputes/disputes.seeder.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { DisputeStatusEnum } from '../../../utils/enums.utils';
 import { faker } from '@faker-js/faker';
+import { DisputeStatusEnum } from '../../../utils/enums.utils';
 import { Dispute, DisputeDocument } from '../../schemas/disputes.schema';
 import { User, UserDocument } from '../../schemas/users.schema';
+
+@Injectable()
+export class DisputesSeeder {
+  constructor(
+    @InjectModel(Dispute.name) private disputeModel: Model<DisputeDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+  ) {}
 
