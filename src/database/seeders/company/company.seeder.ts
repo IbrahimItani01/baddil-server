115bf221f15b10093c7e9988e5522ba15f55f347
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 17:07:41 +0200

    seeders<>imported necessary modules in Company seeder

---
+++ src/database/seeders/company/company.seeder.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { faker } from '@faker-js/faker';
+import { Company, CompanyDocument } from "../../schemas/company.schema";
+
