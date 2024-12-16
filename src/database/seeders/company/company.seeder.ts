028971193c6220e11e1fa9d5d103ee6ca627eb88
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 17:09:05 +0200

    seeders<>generated company data using faker

--- src/database/seeders/company/company.seeder.ts
+++ src/database/seeders/company/company.seeder.ts
@@ -19,3 +19,77 @@ export class CompanySeeder {
       return; 
     }
 
+    const companyData = {
+      name: 'Baddĭl',
+      notification_count: faker.number.int({ min: 0, max: 100 }),
+      flags_count: faker.number.int({ min: 0, max: 50 }),
+      disputes_count: faker.number.int({ min: 0, max: 20 }),
+      users: {
+        admins_count: faker.number.int({ min: 1, max: 10 }),
+        brokers_count: faker.number.int({ min: 5, max: 50 }),
+        barterers_count: faker.number.int({ min: 100, max: 1000 }),
+      },
+      vip_criteria: {
+        min_success_rate: faker.number.float({ min: 80, max: 100 }),
+        min_average_rating: faker.number.float({ min: 4, max: 5 }),
+        min_total_trades: faker.number.int({ min: 50, max: 500 }),
+      },
+      finances: {
+        incomes: {
+          total_revenue: faker.number.float({ min: 10000, max: 100000 }),
+          sources: {
+            subscriptions: {
+              brokers: [],
+              barterers: [],
+            },
+            broker_commissions: {
+              platform_percentage: 5,
+              broker_earnings: [],
+              total_commissions: faker.number.float({ min: 500, max: 5000 }),
+            },
+            partnerships: [],
+          },
+        },
+        expenses: {
+          total_expenses: faker.number.float({ min: 5000, max: 50000 }),
+          details: {
+            broker_payouts: [],
+            maintenance: faker.number.float({ min: 1000, max: 10000 }),
+            operational_costs: {
+              salaries: faker.number.float({ min: 2000, max: 20000 }),
+              office_expenses: faker.number.float({ min: 500, max: 5000 }),
+            },
+          },
+        },
+      },
+      subscription_plans: [
+        {
+          name: 'Pro Plan',
+          target_user_type: 'barterer',
+          monthly_price: faker.number.float({ min: 5, max: 10 }),
+          features: ['Feature A', 'Feature B'],
+        },
+        {
+          name: 'VIP Plan',
+          target_user_type: 'broker',
+          monthly_price: faker.number.float({ min: 15, max: 35}),
+          features: ['Feature C', 'Feature D'],
+        },
+      ],
+      tiers: [
+        {
+          name: 'Bronze',
+          order: 1,
+          min_trades_to_reach: 10,
+          icon_url: faker.image.avatar(),
+        },
+        {
+          name: 'Silver',
+          order: 2,
+          min_trades_to_reach: 50,
+          icon_url: faker.image.avatar(),
+        },
+      ],
+      updated_last_by: new Types.ObjectId(),
+    };
+
