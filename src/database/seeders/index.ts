303229ac49bb05f3d97aaa722554edf162a69fd6
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 17:25:32 +0200

    index<>imported all seeders

--- src/database/seeders/index.ts
+++ src/database/seeders/index.ts
@@ -1,8 +1,13 @@
 
+import { NestFactory } from '@nestjs/core';
+import { CompanySeeder } from './company/company.seeder';
+import { UsersSeeder } from './users/users.seeder';
+import { BarterersSeeder } from './barterers/barterers.seeder';
+import { BrokersSeeder } from './brokers/brokers.seeder';
+import { BartersSeeder } from './barters/barters.seeder';
+import { ChatsSeeder } from './chats/chats.seeder';
+import { DisputesSeeder } from './disputes/disputes.seeder';
+import { FlagsSeeder } from './flags/flags.seeder';
+import { NotificationsSeeder } from './notifications/notifications.seeder';
+import { SeedersModule } from './seeders.module';
 
-
-async function bootstrap() {
-
-}
-
-bootstrap();
