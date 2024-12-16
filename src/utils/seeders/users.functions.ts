9d04f1725602a7d1b5de6c9a7589a11496a3e3cf
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 16:28:27 +0200

    utils<>fixed a logic error in the generate valid password function

--- src/utils/seeders/users.functions.ts
+++ src/utils/seeders/users.functions.ts
@@ -2,14 +2,18 @@ import { faker } from '@faker-js/faker';
 
 export const generateValidPassword = (): string => {
   const minLength = 12;
-  const specialChars = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '=', '+'];
+  
+  const specialChars = ['-', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '=', '+'];
 
   // Function to check if password meets the requirements
   const isValidPassword = (password: string): boolean => {
     const hasUpperCase = /[A-Z]/.test(password);
     const hasLowerCase = /[a-z]/.test(password);
     const hasNumber = /\d/.test(password);
+    
+    // Generate a regular expression that includes all special characters
     const hasSpecialChar = new RegExp(`[${specialChars.join('')}]`).test(password);
+    
     const hasMinLength = password.length >= minLength;
 
     return hasUpperCase && hasLowerCase && hasNumber && hasSpecialChar && hasMinLength;
@@ -29,3 +33,5 @@ export const generateValidPassword = (): string => {
 
   return generatePassword();
 };
+
+
