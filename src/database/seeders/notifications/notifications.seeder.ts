62c59b37795390fc05ea9fbd5e6694adb25efa6c
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 16:31:54 +0200

    seeders<>imported required modules in notifications seeder

---
+++ src/database/seeders/notifications/notifications.seeder.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { faker } from '@faker-js/faker';
+import { User, UserDocument } from '../../schemas/users.schema';
+import {
+  Notification,
+  NotificationDocument,
+} from '../../schemas/notifications.schema';
+
