cd1f1b9a1de06a5893f5d798f3c45366191e37c9
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 16:59:52 +0200

    seeders<>constructed FlagsSeeder

--- src/database/seeders/flags/flags.seeder.ts
+++ src/database/seeders/flags/flags.seeder.ts
@@ -6,3 +6,12 @@ import { FlagTypeEnum, FlagStatusEnum } from '../../../utils/enums.utils';
 import { Flag, FlagDocument } from '../../schemas/flags.schema';
 import { Barter, BarterDocument } from '../../schemas/barters.schema';
 import { User, UserDocument } from '../../schemas/users.schema';
+
+@Injectable()
+export class FlagsSeeder {
+  constructor(
+    @InjectModel(Flag.name) private flagModel: Model<FlagDocument>,
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    @InjectModel(Barter.name) private barterModel: Model<BarterDocument>,
+  ) {}
+
