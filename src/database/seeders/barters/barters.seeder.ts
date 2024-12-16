9760ce55fac4c6d9c14776ac650068c0f3a886ff
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 17:21:04 +0200

    seeders<>generated barters data using faker

--- src/database/seeders/barters/barters.seeder.ts
+++ src/database/seeders/barters/barters.seeder.ts
@@ -20,3 +20,41 @@ export class BartersSeeder {
     return this.barterModel;
   }
 
+  async seed(count: number): Promise<void> {
+    const promises = Array.from({ length: count }).map(async () => {
+      const fakeBarter = {
+        status: faker.helpers.arrayElement(Object.values(BarterStatusEnum)),
+        initiator_items: [new Types.ObjectId(), new Types.ObjectId()],
+        receiver_items: [new Types.ObjectId(), new Types.ObjectId()],
+        completed_at: faker.datatype.boolean() ? faker.date.past() : null,
+
+        rating: faker.datatype.boolean()
+          ? faker.helpers.arrayElement(ratingValues)
+          : null,
+
+        reviews: faker.datatype.boolean()
+          ? [
+              {
+                side: faker.helpers.arrayElement(Object.values(ReviewSideEnum)),
+                review_text: faker.lorem.sentence(),
+              },
+              {
+                side: faker.helpers.arrayElement(Object.values(ReviewSideEnum)),
+                review_text: faker.lorem.sentence(),
+              },
+            ]
+          : [],
+
+        meetup: faker.datatype.boolean()
+          ? {
+              meetup_id: new Types.ObjectId(),
+              status: faker.helpers.arrayElement(
+                Object.values(MeetupStatusEnum),
+              ),
+              location: faker.location.city(),
+              qr_code: uuidv4(),
+              date: faker.date.future(),
+            }
+          : null,
+      };
+
