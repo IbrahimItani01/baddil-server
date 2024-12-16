3f6c54cf919276128e593e15f642ba1d9d689e45
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 17:14:32 +0200

    seeders<>imported necessary modules in broker seeder

---
+++ src/database/seeders/brokers/brokers.seeder.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { faker } from '@faker-js/faker';
+import { Broker, BrokerDocument } from '../../schemas/brokers.schema';
+import { User, UserDocument } from '../../schemas/users.schema';
+import {
+  Earnings,
+  Performance,
+  VipStatus,
+} from '../../subSchemas/brokers.subSchema';
+import {
+  ItemConditionEnum,
+  ClientStatusEnum,
+  RatingEnum,
+} from '../../../utils/enums.utils';
+import { v4 as uuidv4 } from 'uuid';
+import { Barterer, BartererDocument } from '../../schemas/barterers.schema';
+
