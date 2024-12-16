d387a9e5001236b43bd5ce4cd389a435eaeb308c
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-16 17:22:35 +0200

    seeders<>fixed importing pathing

--- src/database/seeders/barterers/barterers.seeder.ts
+++ src/database/seeders/barterers/barterers.seeder.ts
@@ -2,10 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { faker } from '@faker-js/faker';
-import { Barterer, BartererDocument } from 'src/database/schemas/barterers.schema';
-import { User, UserDocument } from 'src/database/schemas/users.schema';
-import { AutoTradeStatusEnum, ItemConditionEnum } from 'src/utils/enums.utils';
-import { Broker, BrokerDocument } from 'src/database/schemas/brokers.schema';
+import { Barterer, BartererDocument } from '../../schemas/barterers.schema';
+import { User, UserDocument } from '../../schemas/users.schema';
+import {
+  AutoTradeStatusEnum,
+  ItemConditionEnum,
+} from '../../../utils/enums.utils';
+import { Broker, BrokerDocument } from '../../schemas/brokers.schema';
 
 @Injectable()
 export class BarterersSeeder {
