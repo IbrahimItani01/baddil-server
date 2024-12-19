d92aeba5b6624e55679845ee5deec8f8fe5b27d9
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-19 02:00:42 +0200

    seeders<>injected models

--- src/database/seeders/barters/barters.seeder.ts
+++ src/database/seeders/barters/barters.seeder.ts
@@ -2,20 +2,22 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
 import { faker } from '@faker-js/faker';
+import { v4 as uuidv4 } from 'uuid';
 import { BarterStatusEnum, RatingEnum } from '../../../utils/enums.utils';
 import { MeetupStatusEnum, ReviewSideEnum } from '../../../utils/enums.utils';
-import { v4 as uuidv4 } from 'uuid';
 import { Barter, BarterDocument } from '../../schemas/barters.schema';
-
-const ratingValues = Object.values(RatingEnum).filter(
-  (value) => typeof value === 'number',
-) as number[];
+import { Broker, BrokerDocument } from '../../schemas/brokers.schema';
+import { Barterer, BartererDocument } from '../../schemas/barterers.schema';
 
 @Injectable()
 export class BartersSeeder {
   constructor(
     @InjectModel(Barter.name)
     private readonly barterModel: Model<BarterDocument>,
+    @InjectModel(Broker.name)
+    private readonly brokerModel: Model<BrokerDocument>,
+    @InjectModel(Barterer.name)
+    private readonly bartererModel: Model<BartererDocument>,
   ) {}
 
   getModel(): Model<BarterDocument> {
