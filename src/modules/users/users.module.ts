847573da55f780f49b4284cc6dd29c7e31cf309a
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-21 04:33:21 +0200

    modules<>imported prisma service

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,15 +1,15 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
-import { User, UserSchema } from '../../database/schemas/users.schema';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
-  ],
+  imports: [],  
   controllers: [UsersController],
-  providers: [UsersService],
-  exports: [UsersService],
+  providers: [
+    UsersService,
+    PrismaService,  
+  ],
+  exports: [UsersService],  
 })
 export class UsersModule {}
