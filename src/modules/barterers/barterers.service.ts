d5aeadd8a67edced74797706bd924d2fcef80964
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-21 04:33:47 +0200

    services<>cleared services

--- src/modules/barterers/barterers.service.ts
+++ src/modules/barterers/barterers.service.ts
@@ -1,21 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types } from 'mongoose';
-import { Barterer, BartererDocument } from '../../database/schemas/barterers.schema';
 
 @Injectable()
 export class BarterersService {
   constructor(
-    @InjectModel(Barterer.name) private bartererModel: Model<BartererDocument>,
   ) {}
 
-  async create(userId: Types.ObjectId): Promise<Barterer> {
-    const newBarterer = new this.bartererModel({ user_id:userId });
-    return newBarterer.save();
-  }
-  
-
-  async findByUserId(userId: Types.ObjectId) {
-    return this.bartererModel.findOne({ user_id: userId }).exec();
-  }
 }
--- src/modules/brokers/brokers.service.ts
+++ src/modules/brokers/brokers.service.ts
@@ -1,20 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types } from 'mongoose';
-import { Broker, BrokerDocument } from '../../database/schemas/brokers.schema';
 
 @Injectable()
 export class BrokersService {
-  constructor(
-    @InjectModel(Broker.name) private brokerModel: Model<BrokerDocument>,
-  ) {}
-
-  async create(userId: Types.ObjectId): Promise<Broker> {
-    const newBroker = new this.brokerModel({ user_id:userId });
-    return newBroker.save();
-  }
-
-  async findByUserId(userId: Types.ObjectId) {
-    return this.brokerModel.findOne({ user_id: userId }).exec();
-  }
+  constructor() {}
 }
