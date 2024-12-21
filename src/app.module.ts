143be974356a059743313f54f1f72d17c08dfb55
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-21 04:31:42 +0200

    modules<>removed unused modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { FirebaseAdminModule } from './auth/firebase/firebase.module';
 import { UsersModule } from './modules/users/users.module';
-import { SeedersModule } from './database/seeders/seeders.module';
 
 @Module({
   imports: [
@@ -14,10 +12,8 @@ import { SeedersModule } from './database/seeders/seeders.module';
       isGlobal: true,
     }),
     AuthModule,
-    DatabaseModule,
     FirebaseAdminModule,
     UsersModule,
-    SeedersModule
   ],
   controllers: [AppController],
   providers: [AppService],
