ea2b62b4ce44682e9c54a7540e27302254a6c37d
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-21 02:06:18 +0200

    seeders<>declared seedExpenses

---
+++ src/database/seeders/seeds/expenses.seed.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+const prisma = new PrismaClient();
+
+export const seedExpenses = async () => {
+  console.log('Seeding Expenses...');
+  await Promise.all(
+    Array.from({ length: 50 }).map(() =>
+      prisma.expense.create({
+        data: {
+          amount: parseFloat(
+            faker.finance.amount({ min: 100, max: 5000, dec: 2 }),
+          ),
+          description: faker.commerce.productDescription(),
+          expense_type: faker.helpers.arrayElement([
+            'salary',
+            'broker_compensation',
+            'other',
+          ]),
+          date: faker.date.past(),
+        },
+      }),
+    ),
+  );
+};
