7812f9a3634c12a04f0efa1d78600e56d508b712
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-21 05:37:38 +0200

    utils<>exported function to getUserType value by Id

--- src/utils/modules/users/users.utils.ts
+++ src/utils/modules/users/users.utils.ts
@@ -15,3 +15,20 @@ export const getUserTypeId = async (
 
   return userTypeRecord.id;
 };
+export const getUserTypeById = async (
+  prisma: PrismaService,
+  userTypeId: number,
+) => {
+  const userTypeRecord = await prisma.userType.findUnique({
+    where: { id: userTypeId },
+  });
+
+  if (!userTypeRecord) {
+    throw new BadRequestException(
+      `User type with ID "${userTypeId}" not found`,
+    );
+  }
+
+  return userTypeRecord.type;
+};
+
