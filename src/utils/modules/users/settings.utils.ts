445e79e04aafd67c5cfc42f8607929af8e0f1948
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-21 05:39:04 +0200

    utils<>exported function to create user settings data

--- src/utils/modules/users/settings.utils.ts
+++ src/utils/modules/users/settings.utils.ts
@@ -8,3 +8,28 @@ interface SettingsData {
   notifications?: boolean;
 }
 
+export async function createUserSettings(
+  prisma: PrismaService,
+  language: string = 'english',
+  theme: string = 'light',
+) {
+  const validLanguages: Language[] = ['english', 'french'];
+  const validThemes: Theme[] = ['dark', 'light'];
+
+  const finalLanguage = validLanguages.includes(language as Language)
+    ? (language as Language)
+    : 'english';
+  const finalTheme = validThemes.includes(theme as Theme)
+    ? (theme as Theme)
+    : 'light';
+
+  const newSetting = await prisma.setting.create({
+    data: {
+      language: finalLanguage,
+      theme: finalTheme,
+      notifications: true,
+    },
+  });
+
+  return newSetting;
+}
