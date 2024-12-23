62e0effbe90d8e5b71224e1ed0aa4544f6ace6ba
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 05:25:57 +0200

    controllers<>constructed ai controller

--- src/modules/ai/ai.controller.ts
+++ src/modules/ai/ai.controller.ts
@@ -11,3 +11,9 @@ import { AIService } from './ai.service';
 import { JwtAuthGuard } from 'src/guards/jwt.guard';
 import { AllowedUserTypes, UserTypeGuard } from 'src/guards/userType.guard';
 
+@UseGuards(JwtAuthGuard, UserTypeGuard)
+@AllowedUserTypes('barterer', 'broker')
+@Controller('ai')
+export class AIController {
+  constructor(private readonly aiService: AIService) {}
+
