9f0a2ae11fda37b1f7bc3c3d7ef546d828df93be
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 03:01:53 +0200

    utils<>imported required packages in config file for files

---
+++ src/utils/modules/config/file-upload.config.ts
@@ -0,0 +1,5 @@
+import { diskStorage } from 'multer';
+import * as path from 'path';
+import { BadRequestException } from '@nestjs/common';
+import { CustomRequest } from '../interface/custom-request.interface';
+
