428d3c4ffa34f68b41161bbc78028bfcc2670eab
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 05:24:32 +0200

    controllers<>imported wallets services

---
+++ src/modules/wallets/wallets.controller.ts
@@ -0,0 +1,21 @@
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  Put,
+  Delete,
+  UseGuards,
+  UseInterceptors,
+  Req,
+  UploadedFiles,
+  BadRequestException,
+} from '@nestjs/common';
+import { WalletsService } from './wallets.service';
+import { JwtAuthGuard } from 'src/guards/jwt.guard';
+import { AllowedUserTypes, UserTypeGuard } from 'src/guards/userType.guard';
+import { FilesInterceptor } from '@nestjs/platform-express';
+import { itemImagesUploadOptions } from 'src/utils/modules/config/file-upload.config';
+import { ItemCondition } from '@prisma/client';
+
