50c695e87ea18e913f597b38401469ce15629fff
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 05:23:22 +0200

    controllers<>imported brokers services

--- src/modules/brokers/brokers.controller.ts
+++ src/modules/brokers/brokers.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Request,
+  HttpException,
+  HttpStatus,
+  Delete,
+  Body,
+  Post,
+  Param,
+} from '@nestjs/common';
+import { BrokerService } from './brokers.service';
+import { JwtAuthGuard } from 'src/guards/jwt.guard';
+import { UserTypeGuard, AllowedUserTypes } from 'src/guards/userType.guard';
 
 @Controller('brokers')
 export class BrokersController {}
