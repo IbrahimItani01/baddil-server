ee78e6f74938f51a039a511cf1dca19f6f55b0cd
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 03:11:21 +0200

    services<>constructed tiers  service

--- src/modules/tiers/tiers.service.ts
+++ src/modules/tiers/tiers.service.ts
@@ -1,3 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma.service';
 
+@Injectable()
+export class TiersService {
+  constructor(private readonly prisma: PrismaService) {}
