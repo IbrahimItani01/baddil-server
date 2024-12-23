fe7569154af5e8f70f1be43d17467a4bf57b806f
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 02:53:03 +0200

    modules<>imported serve static module to set uploads route to save files in server

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,17 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { FirebaseAdminModule } from './auth/firebase/firebase.module';
 import { UsersModule } from './modules/users/users.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads', // Files will be accessible at /uploads/<filename>
     }),
     AuthModule,
     FirebaseAdminModule,
