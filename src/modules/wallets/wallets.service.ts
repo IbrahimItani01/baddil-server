ee1faa89ef1394d615d3af1bbaffe84a237e7b56
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 03:08:05 +0200

    services<>imported necessary packages and services in wallets service

---
+++ src/modules/wallets/wallets.service.ts
@@ -0,0 +1,5 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { ItemCondition } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+import * as fs from 'fs';
+import * as path from 'path';
