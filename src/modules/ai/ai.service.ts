5bbc562b348357304c1c2e7af474af75c6ffd474
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 03:10:33 +0200

    services<>constructed ai service

--- src/modules/ai/ai.service.ts
+++ src/modules/ai/ai.service.ts
@@ -2,3 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { BarterStatus } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
+@Injectable()
+export class AIService {
+  constructor(private readonly prisma: PrismaService) {}
+
