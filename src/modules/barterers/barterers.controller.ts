1f423a54eb1f06e64069a57c773b3261a48099ed
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 05:22:54 +0200

    controllers<>imported barterers services

--- src/modules/barterers/barterers.controller.ts
+++ src/modules/barterers/barterers.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Request,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { BarterersService } from './barterers.service';
+import { JwtAuthGuard } from 'src/guards/jwt.guard';
+import { UserTypeGuard, AllowedUserTypes } from 'src/guards/userType.guard';
 
 @Controller('barterers')
 export class BarterersController {}
