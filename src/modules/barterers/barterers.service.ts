ee7ceb34a2d89c139d5d2757d604add141825bbd
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 03:09:13 +0200

    services<>imported necessary packages and services in barterers service

--- src/modules/barterers/barterers.service.ts
+++ src/modules/barterers/barterers.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class BarterersService {
