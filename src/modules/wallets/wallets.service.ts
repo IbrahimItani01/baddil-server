1fd39a18e93eeca296b1c60d9e32412fb4c97963
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 03:10:23 +0200

    services<>constructed wallets service

--- src/modules/wallets/wallets.service.ts
+++ src/modules/wallets/wallets.service.ts
@@ -3,3 +3,7 @@ import { ItemCondition } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 import * as fs from 'fs';
 import * as path from 'path';
+@Injectable()
+export class WalletsService {
+  constructor(private readonly prisma: PrismaService) {}
+
