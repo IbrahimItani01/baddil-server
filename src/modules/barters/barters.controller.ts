7dab63f2b78ae02fa6051887aa1fd0104cf4974e
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 05:26:16 +0200

    controllers<>constructed barters controller

--- src/modules/barters/barters.controller.ts
+++ src/modules/barters/barters.controller.ts
@@ -5,3 +5,9 @@ import { BartersService } from './barters.service';
 import { BarterStatus } from '@prisma/client';
 
 
+@Controller('barters')
+@UseGuards(JwtAuthGuard, UserTypeGuard)
+@AllowedUserTypes('barterer')
+export class BartersController {
+  constructor(private readonly barterService: BartersService) {}
+
