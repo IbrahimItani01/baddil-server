1da0ba14fe96dabded492cc0f392636b84587059
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 03:10:56 +0200

    services<>constructed barters service

--- src/modules/barters/barters.service.ts
+++ src/modules/barters/barters.service.ts
@@ -2,3 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { BarterStatus } from '@prisma/client';
 import { PrismaService } from 'src/database/prisma.service';
 
+@Injectable()
+export class BartersService {
+  constructor(private readonly prisma: PrismaService) {}
+
