636760519f4dff971544eaaec70f31f60ba638d1
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-23 05:19:35 +0200

    services<>declared getBarterByUser method

--- src/modules/barters/barters.service.ts
+++ src/modules/barters/barters.service.ts
@@ -6,3 +6,14 @@ import { PrismaService } from 'src/database/prisma.service';
 export class BartersService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async getBartersByUser(userId: string) {
+    try {
+      return await this.prisma.barter.findMany({
+        where: { user1_id: userId },
+      });
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    } catch (error) {
+      throw new Error('Failed to fetch barters');
+    }
+  }
+
