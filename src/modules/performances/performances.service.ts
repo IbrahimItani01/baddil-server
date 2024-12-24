fa59a41767dce2dd45744647d1d09d5ebc2c88e3
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-24 11:42:55 +0200

    services<>constructed performance service

---
+++ src/modules/performances/performances.service.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class PerformancesService {
+    constructor(private readonly prisma: PrismaService) {}
+
