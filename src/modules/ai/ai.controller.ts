05e48217676ab58565ae731b7e15eb40b42dd068
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-24 10:59:22 +0200

    controllers<>allowed admins to access ai apis

--- src/modules/ai/ai.controller.ts
+++ src/modules/ai/ai.controller.ts
@@ -6,13 +6,15 @@ import {
   Body,
   Put,
   UseGuards,
+  HttpException,
+  HttpStatus,
 } from '@nestjs/common';
 import { AIService } from './ai.service';
 import { JwtAuthGuard } from 'src/guards/jwt.guard';
 import { AllowedUserTypes, UserTypeGuard } from 'src/guards/userType.guard';
 
 @UseGuards(JwtAuthGuard, UserTypeGuard)
-@AllowedUserTypes('barterer', 'broker')
+@AllowedUserTypes('barterer', 'broker', 'admin')
 @Controller('ai')
 export class AIController {
   constructor(private readonly aiService: AIService) {}
