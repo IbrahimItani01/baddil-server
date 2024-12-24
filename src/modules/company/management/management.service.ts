4a4eb6734d1aa99665c4c056e14c7f9e66e6c279
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-24 11:36:41 +0200

    services<>declared createSubscriptionPlan method

--- src/modules/company/management/management.service.ts
+++ src/modules/company/management/management.service.ts
@@ -5,3 +5,21 @@ import { PrismaService } from 'src/database/prisma.service';
 export class ManagementService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async createSubscriptionPlan(data: {
+    name: string;
+    price: number;
+    targetUserTypeId: string; // Updated to handle foreign key
+    criteria?: string;
+  }) {
+    return this.prisma.subscriptionPlan.create({
+      data: {
+        name: data.name,
+        price: data.price,
+        criteria: data.criteria,
+        user_type: {
+          connect: { id: data.targetUserTypeId }, // Relates to UserType by ID
+        },
+      },
+    });
+  }
+
