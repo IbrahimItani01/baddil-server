54d920a4afab5819aa3715bf2383a557540ef327
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-24 11:09:18 +0200

    controllers<>constructed management controller class

---
+++ src/modules/company/management/management.controller.ts
@@ -0,0 +1,20 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Body,
+  Param,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
+import { ManagementService } from './management.service';
+import { JwtAuthGuard } from 'src/guards/jwt.guard';
+import { AllowedUserTypes, UserTypeGuard } from 'src/guards/userType.guard';
+
+@UseGuards(JwtAuthGuard, UserTypeGuard)
+@AllowedUserTypes('admin')
+@Controller('management')
+export class ManagementController {
+  constructor(private readonly managementService: ManagementService) {}
+
