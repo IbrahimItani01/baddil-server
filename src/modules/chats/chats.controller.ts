a527256d0f6505627c78e1c5886fd9fd088cbb5c
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-24 11:04:36 +0200

    controllers<>constructed chats controller with guards

--- src/modules/chats/chats.controller.ts
+++ src/modules/chats/chats.controller.ts
@@ -14,3 +14,8 @@ import { ChatsService } from './chats.service';
 import { JwtAuthGuard } from 'src/guards/jwt.guard';
 import { AllowedUserTypes, UserTypeGuard } from 'src/guards/userType.guard';
 
+@UseGuards(JwtAuthGuard, UserTypeGuard)
+@Controller('chats')
+export class ChatsController {
+  constructor(private readonly chatsService: ChatsService) {}
+
