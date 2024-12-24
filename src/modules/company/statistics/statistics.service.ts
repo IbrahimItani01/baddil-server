c31ec8fea6e41ec878a3ca48533cc3bca88dcc7a
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-24 11:39:47 +0200

    services<>declared getUserCounts method

--- src/modules/company/statistics/statistics.service.ts
+++ src/modules/company/statistics/statistics.service.ts
@@ -6,3 +6,25 @@ import { getUserTypeId } from 'src/utils/modules/users/users.utils';
 export class StatisticsService {
   constructor(private readonly prisma: PrismaService) {}
 
+  async getUserCounts(): Promise<{
+    admins_count: number;
+    brokers_count: number;
+    barterers_count: number;
+  }> {
+    const adminTypeId = await getUserTypeId(this.prisma, 'admin');
+    const brokerTypeId = await getUserTypeId(this.prisma, 'broker');
+    const bartererTypeId = await getUserTypeId(this.prisma, 'barterer');
+
+    const [adminsCount, brokersCount, barterersCount] = await Promise.all([
+      this.prisma.user.count({ where: { user_type_id: adminTypeId } }),
+      this.prisma.user.count({ where: { user_type_id: brokerTypeId } }),
+      this.prisma.user.count({ where: { user_type_id: bartererTypeId } }),
+    ]);
+
+    return {
+      admins_count: adminsCount,
+      brokers_count: brokersCount,
+      barterers_count: barterersCount,
+    };
+  }
+}
