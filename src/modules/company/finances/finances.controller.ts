fd36f204e784c3710aa1c0386c1413347624828f
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-24 11:07:39 +0200

    controllers<>constructed finances controller

---
+++ src/modules/company/finances/finances.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Post, Body, Query, UseGuards } from '@nestjs/common';
+import { FinancesService } from './finances.service';
+import { ExpenseType, ProfitSource } from '@prisma/client';
+import { JwtAuthGuard } from 'src/guards/jwt.guard';
+import { AllowedUserTypes, UserTypeGuard } from 'src/guards/userType.guard';
+
+@UseGuards(JwtAuthGuard, UserTypeGuard)
+@AllowedUserTypes('admin')
+@Controller('finances')
+export class FinancesController {
+  constructor(private readonly financesService: FinancesService) {}
+
