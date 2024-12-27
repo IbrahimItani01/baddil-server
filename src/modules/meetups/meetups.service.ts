3bd5303f140a4939a0c1220603ea2a8c75298759
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-27 03:20:40 +0200

    services<>imported required packages in meetups

--- src/modules/meetups/meetups.service.ts
+++ src/modules/meetups/meetups.service.ts
@@ -1,4 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  InternalServerErrorException,
+  BadRequestException,
+} from '@nestjs/common'; // 📦 Importing necessary exceptions
+import { Meetup, MeetupStatus } from '@prisma/client'; // 📅 Importing Meetup and MeetupStatus types from Prisma
+import { PrismaService } from 'src/database/prisma.service'; // 🗄️ Importing PrismaService for database access
 
 @Injectable()
 export class MeetupsService {}
