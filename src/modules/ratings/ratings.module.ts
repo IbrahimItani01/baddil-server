ce4d838776644b57f61338956904ce81bd02faa6
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-27 03:06:08 +0200

    modules<>commented ratings module

--- src/modules/ratings/ratings.module.ts
+++ src/modules/ratings/ratings.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { RatingsService } from './ratings.service';
-import { RatingsController } from './ratings.controller';
+import { Module } from '@nestjs/common'; // 📦 Importing the Module decorator
+import { RatingsService } from './ratings.service'; // 📊 Importing the RatingsService
+import { RatingsController } from './ratings.controller'; // 📋 Importing the RatingsController
+import { PrismaService } from 'src/database/prisma.service'; // 🗄️ Importing PrismaService for database access
 
 @Module({
-  controllers: [RatingsController],
-  providers: [RatingsService],
+  controllers: [RatingsController], // Registering the RatingsController
+  providers: [RatingsService, PrismaService], // Registering the RatingsService and PrismaService
 })
 export class RatingsModule {}
