db187b9d0e2e2ff65bc3a4a6beb46e7281e936dd
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-27 03:06:16 +0200

    modules<>commented tiers module

--- src/modules/tiers/tiers.module.ts
+++ src/modules/tiers/tiers.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { TiersService } from './tiers.service';
-import { TiersController } from './tiers.controller';
+import { Module } from '@nestjs/common'; // 📦 Importing the Module decorator
+import { TiersService } from './tiers.service'; // 📊 Importing the TiersService
+import { TiersController } from './tiers.controller'; // 📋 Importing the TiersController
+import { PrismaService } from 'src/database/prisma.service'; // 🗄️ Importing PrismaService for database access
 
 @Module({
-  controllers: [TiersController],
-  providers: [TiersService],
+  controllers: [TiersController], // Registering the TiersController
+  providers: [TiersService, PrismaService], // Registering the TiersService and PrismaService
 })
 export class TiersModule {}
