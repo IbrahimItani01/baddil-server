e77d3d8d5a452f64367b0fdc0675c726e85a214f
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-27 03:22:15 +0200

    services<>declared method createLocation

--- src/modules/locations/locations.service.ts
+++ src/modules/locations/locations.service.ts
@@ -9,3 +9,32 @@ import { PrismaService } from 'src/database/prisma.service'; // 🗄️ Importin
 @Injectable()
 export class LocationsService {
   constructor(private readonly prisma: PrismaService) {} // 🏗️ Injecting PrismaService
+
+  /**
+   * ➕ Create a new location
+   * @param name - The name of the location.
+   * @param longitude - The longitude of the location.
+   * @param latitude - The latitude of the location.
+   * @returns The created location record.
+   * @throws InternalServerErrorException if there is an error creating the location.
+   */
+  async createLocation(
+    name: string,
+    longitude: number,
+    latitude: number,
+  ): Promise<Location> {
+    try {
+      return await this.prisma.location.create({
+        data: {
+          name,
+          longitude,
+          latitude,
+        },
+      });
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Failed to create location: ' + error.message,
+      ); // 🚫 Error handling
+    }
+  }
+
