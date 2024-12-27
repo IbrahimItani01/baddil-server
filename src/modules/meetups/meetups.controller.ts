0467aa9465576233e7db3eee7cb720b848cfeb19
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-27 03:13:25 +0200

    controllers<>imported required packages in meetups

--- src/modules/meetups/meetups.controller.ts
+++ src/modules/meetups/meetups.controller.ts
@@ -1,5 +1,16 @@
-import { Controller } from '@nestjs/common';
-import { MeetupsService } from './meetups.service';
+import {
+  Controller,
+  Post,
+  Body,
+  Param,
+  UseGuards,
+  Get,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common'; // 📦 Importing necessary decorators and exceptions
+import { MeetupsService } from './meetups.service'; // 📅 Importing MeetupsService for business logic
+import { AllowedUserTypes, UserTypeGuard } from 'src/guards/userType.guard'; // 🛡️ Importing user type guards
+import { JwtAuthGuard } from 'src/guards/jwt.guard'; // 🔑 Importing JWT authentication guard
 
 @Controller('meetups')
 export class MeetupsController {
