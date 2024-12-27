5ad4c8e077e2eeb2dfe3c38ce671e8d33d7f67b6
Ibrahim Itani <I.itani2002@gmail.com>
2024-12-27 03:06:30 +0200

    modules<>commented wallets module

--- src/modules/wallets/wallets.module.ts
+++ src/modules/wallets/wallets.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { WalletsService } from './wallets.service';
-import { WalletsController } from './wallets.controller';
+import { Module } from '@nestjs/common'; // 🌐 Importing the Module decorator from NestJS
+import { WalletsService } from './wallets.service'; // 💼 Importing the service that handles wallet logic
+import { WalletsController } from './wallets.controller'; // 📜 Importing the controller that handles wallet-related HTTP requests
+import { PrismaService } from 'src/database/prisma.service';
 
 @Module({
-  controllers: [WalletsController],
-  providers: [WalletsService],
+  controllers: [WalletsController], // 👨‍💻 Registering the WalletsController to handle HTTP routes for wallets
+  providers: [WalletsService, PrismaService], // 🛠️ Registering the WalletsService and PrismaService as providers
 })
-export class WalletModule {}
+export class WalletModule {} // 🏦 The WalletModule is now ready to be used in your NestJS app
