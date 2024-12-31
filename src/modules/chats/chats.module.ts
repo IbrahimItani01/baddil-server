4114fa7176848622234affb9f05884aa2df81264
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-01 01:16:16 +0200

    modules<>provided services in chats module

--- src/modules/chats/chats.module.ts
+++ src/modules/chats/chats.module.ts
@@ -3,9 +3,12 @@ import { ChatsService } from './chats.service'; // 💬 Importing ChatsService f
 import { ChatsController } from './chats.controller'; // 🎮 Importing ChatsController for handling requests
 import { ChatGateway } from 'src/gateways/chat.gateway'; // 🌐 Importing ChatGateway for WebSocket communication
 import { PrismaService } from 'src/database/prisma.service'; // 🗄️ Importing PrismaService for database access
+import { MessagesService } from '../messages/messages.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
+  imports: [],
   controllers: [ChatsController], // 🎮 Registering the ChatsController
-  providers: [ChatsService, ChatGateway, PrismaService], // 🛠️ Registering the ChatsService, ChatGateway, and PrismaService
+  providers: [ChatsService, ChatGateway, PrismaService, MessagesService,JwtService], // 🛠️ Registering the ChatsService, ChatGateway, and PrismaService
 })
 export class ChatsModule {} // 📦 Chats Module
