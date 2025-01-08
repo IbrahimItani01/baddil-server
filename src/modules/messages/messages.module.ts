1a8f194dfc0296a77b2dafb643a4082831e719db
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-08 06:19:51 +0200

    modules<>handled possible circular dependency issue

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -1,14 +1,23 @@
-import { Module } from '@nestjs/common'; // 📦 Importing necessary decorators
+import { forwardRef, Module } from '@nestjs/common'; // 📦 Importing necessary decorators
 import { MessagesService } from './messages.service'; // 💬 Importing MessagesService for business logic
 import { MessagesController } from './messages.controller'; // 📜 Importing MessagesController for handling requests
-import { ChatsModule } from '../chats/chats.module'; // 💬 Importing ChatsModule for chat-related functionality
 import { PrismaService } from 'src/database/prisma.service'; // 🗄️ Importing PrismaService for database access
 import { ChatGateway } from 'src/gateways/chat.gateway';
 import { JwtService } from '@nestjs/jwt';
+import { ChatsService } from '../chats/chats.service';
+import { ChatsModule } from '../chats/chats.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ChatsModule], // 📦 Importing ChatsModule
+  imports: [ConfigModule, forwardRef(() => ChatsModule)], // Forward reference to prevent circular import
   controllers: [MessagesController], // 🎮 Registering the MessagesController
-  providers: [MessagesService, PrismaService,ChatGateway,JwtService], // 🛠️ Registering the MessagesService and PrismaService
+  providers: [
+    MessagesService,
+    PrismaService,
+    ChatGateway,
+    ChatsService,
+    JwtService,
+  ], // 🛠️ Registering the MessagesService and PrismaService
+  exports: [MessagesService],
 })
 export class MessagesModule {} // 📦 Messages Module
