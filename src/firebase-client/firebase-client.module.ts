b2f0bcdc7437b255b246d25eff28b5fb6f1e47d4
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-08 22:00:46 +0200

    firebase<>deleted firebase client related features

--- src/firebase-client/firebase-client.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FirebaseClientService } from './firebase-client.service';
-import { FirebaseClientController } from './firebase-client.controller';
-
-@Module({
-  controllers: [FirebaseClientController],
-  providers: [FirebaseClientService],
-})
-export class FirebaseClientModule {}
