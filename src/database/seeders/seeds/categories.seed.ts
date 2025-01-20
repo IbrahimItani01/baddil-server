7b57292e5eb3939a409e6c1f72cb7b80016db3d1
Ibrahim Itani <I.itani2002@gmail.com>
2025-01-21 01:21:59 +0200

    seeders<>fixed seeders fto seed database with fixed data

--- src/database/seeders/seeds/categories.seed.ts
+++ src/database/seeders/seeds/categories.seed.ts
@@ -1,15 +1,80 @@
 import { PrismaClient } from '@prisma/client';
-import { faker } from '@faker-js/faker';
 
 export const seedCategories = async (prisma: PrismaClient) => {
   console.log('Seeding Categories...');
 
+  const currentDate = new Date();
+
+  const categoriesData = [
+    {
+      id: '41789d75-b996-400f-8fdc-c87bbf6a6e98',
+      name: 'sports',
+      category_icon: 'futbol',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: '4622df59-2533-4d0b-9ec7-068085ddea1d',
+      name: 'appliances',
+      category_icon: 'blender-phone',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: '63a95c23-7a7a-4de1-9780-5144cfb2ed47',
+      name: 'vehicles',
+      category_icon: 'car',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: '8b506853-22f2-4218-9697-2bfac3c67aec',
+      name: 'electronics',
+      category_icon: 'tablet-alt',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: 'a9a75a53-750e-48f3-9db7-5bd66f87206b',
+      name: 'furniture',
+      category_icon: 'couch',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: 'cfa781cd-809f-4c64-a604-e33d618c481f',
+      name: 'fashion',
+      category_icon: 'tshirt',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: 'd49176c1-b363-409c-86da-f00b5cb33391',
+      name: 'instruments',
+      category_icon: 'music',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: 'f2457de9-ef60-43b1-9509-929722a9d704',
+      name: 'books',
+      category_icon: 'book',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+  ];
+
   await Promise.all(
-    Array.from({ length: 5 }).map(() =>
-      prisma.category.create({
-        data: {
-          name: faker.commerce.department(),
-          category_icon: faker.image.url(), // Updated to use imageUrl()
+    categoriesData.map((category) =>
+      prisma.category.upsert({
+        where: { id: category.id },
+        update: {},
+        create: {
+          id: category.id,
+          name: category.name,
+          category_icon: category.category_icon,
+          created_at: currentDate,
+          updated_at: currentDate,
         },
       }),
     ),
--- src/database/seeders/seeds/subCategories.seed.ts
+++ src/database/seeders/seeds/subCategories.seed.ts
@@ -1,27 +1,83 @@
 import { PrismaClient } from '@prisma/client';
-import { faker } from '@faker-js/faker';
 
 export const seedSubcategories = async (prisma: PrismaClient) => {
   console.log('Seeding Subcategories...');
 
-  // Fetch existing categories to associate subcategories with them
-  const categories = await prisma.category.findMany();
+  const currentDate = new Date();
 
-  if (categories.length === 0) {
-    throw new Error('No categories found. Seed categories first.');
-  }
+  const subcategoriesData = [
+    {
+      id: '09a4b4ee-52a7-437f-b030-ded6ec73b91e',
+      name: 'cars',
+      main_category_id: '63a95c23-7a7a-4de1-9780-5144cfb2ed47',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: '2d202aca-05ed-4d9d-9139-fc09866e72e6',
+      name: 'motorcycles',
+      main_category_id: '63a95c23-7a7a-4de1-9780-5144cfb2ed47',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: '4526ceb4-d085-4d2b-bf9d-ce578df726d6',
+      name: 'trucks',
+      main_category_id: '63a95c23-7a7a-4de1-9780-5144cfb2ed47',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: '4d92896d-0496-4a41-b748-acb38d27ab90',
+      name: 'bicycles',
+      main_category_id: '63a95c23-7a7a-4de1-9780-5144cfb2ed47',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: '4e87550f-2b53-4a6c-af61-3eb1f73e492a',
+      name: 'phones',
+      main_category_id: '8b506853-22f2-4218-9697-2bfac3c67aec',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: '77407e65-fe3e-4c17-ace5-d74e50c7c1b6',
+      name: 'laptops',
+      main_category_id: '8b506853-22f2-4218-9697-2bfac3c67aec',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: '8897fa5f-2a03-46fa-b760-c95513bf9695',
+      name: 'televisions',
+      main_category_id: '8b506853-22f2-4218-9697-2bfac3c67aec',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+    {
+      id: 'c122b0d6-7580-4ab8-953d-dac277437062',
+      name: 'cameras',
+      main_category_id: '8b506853-22f2-4218-9697-2bfac3c67aec',
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    },
+  ];
 
-  // Generate random subcategories
   await Promise.all(
-    Array.from({ length: 10 }).map(() => {
-      const randomCategory = faker.helpers.arrayElement(categories); // Pick a random category
-      return prisma.subcategory.create({
-        data: {
-          name: faker.commerce.productName(), // Generate a random subcategory name
-          main_category_id: randomCategory.id, // Assign a category to the subcategory
+    subcategoriesData.map((subcategory) =>
+      prisma.subcategory.upsert({
+        where: { id: subcategory.id },
+        update: {},
+        create: {
+          id: subcategory.id,
+          name: subcategory.name,
+          main_category_id: subcategory.main_category_id,
+          created_at: currentDate,
+          updated_at: currentDate,
         },
-      });
-    }),
+      }),
+    ),
   );
 
   console.log('Subcategories seeded successfully.');
